77d4c3f8932196837c9cf2c13c4720d69aa5098f
Denis Garifyanov <daneegar@gmail.com>
2019-01-06 08:09:19 +0500

    Initial Commit

---
+++ TinkoffTimeLine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinkoffTimeLine
+//
+//  Created by Denis Garifyanov on 06/01/2019.
+//  Copyright © 2019 Denis Garifyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
